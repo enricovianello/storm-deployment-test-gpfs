296269b06cb50c9e2564a96435f289d382fb065c
Enrico Vianello <maverick17586@gmail.com>
2018-05-30 11:04:28 +0200

    Update create-vm.tf

--- create-vm.tf
+++ create-vm.tf
@@ -26,3 +26,42 @@ resource "openstack_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "${FIP_ADDRESS}"
   instance_id = "${openstack_compute_instance_v2.test.id}"
 }
+
+# Upload configuration scripts
+resource "null_resource" "configure" {
+  connection {
+    type = "ssh"
+  	user = "centos"
+  	agent = false
+  	private_key = "${JENKINS_SSH_KEY_FILE}"
+  	host = "${FIP_ADDRESS}"
+  }
+  
+  provisioner "file" {
+  	source = "conf/"
+  	destination = "/home/centos"
+  }
+  
+  depends_on = [
+    "openstack_compute_floatingip_associate_v2.fip_1",
+  ]
+}
+
+# Boostrap as GPFS cluster client
+resource "null_resource" "bootstrap-gpfs" {
+  connection {
+    type = "ssh"
+        user = "centos"
+        agent = false
+        private_key = "${JENKINS_SSH_KEY_FILE}"
+        host = "${FIP_ADDRESS}"
+  }
+  
+  provisioner "remote-exec" {
+        inline = "sudo sh bootstrap-gpfs.sh"
+  }
+
+  depends_on = [
+    "null_resource.configure",
+  ]
+}
