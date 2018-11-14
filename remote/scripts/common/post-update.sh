3b07c6fd48ea8e252c65dcf0c1078d1c40d45b60
enricovianello <maverick17586@gmail.com>
2018-11-14 12:17:02 +0100

    Fixed missing newline at the end of syscinfig/storm-webdav

--- remote/scripts/common/post-update.sh
+++ remote/scripts/common/post-update.sh
@@ -17,4 +17,5 @@ fi
 # 1.11.15
 
 # StoRM WebDAV needs to know what host has not to be considered as a 3rd-party copy
+echo "" >> /etc/sysconfig/storm-webdav
 echo "STORM_WEBDAV_HOSTNAME_0=\"cloud-vm127.cloud.cnaf.infn.it\"" >> /etc/sysconfig/storm-webdav
