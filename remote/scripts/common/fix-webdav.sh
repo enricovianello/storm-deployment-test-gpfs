924ee309fb1d8cfaf2f248db5cfc95be29b7c5e9
enricovianello <maverick17586@gmail.com>
2019-02-28 18:49:15 +0100

    Fixed WebDAV local hostname alias as cloud-vm127.cloud.cnaf.infn.it

--- remote/scripts/common/fix-webdav.sh
+++ remote/scripts/common/fix-webdav.sh
@@ -4,4 +4,4 @@ trap "exit 1" TERM
 
 # StoRM WebDAV needs to know what host has not to be considered as a 3rd-party copy
 echo ""  >> /etc/sysconfig/storm-webdav
-echo "STORM_WEBDAV_HOSTNAME_0=\"storm.example\"" >> /etc/sysconfig/storm-webdav
+echo "STORM_WEBDAV_HOSTNAME_0=\"cloud-vm127.cloud.cnaf.infn.it\"" >> /etc/sysconfig/storm-webdav
